0ec1f719ef7f55bc2ac3b73c6d7b43b4743e6535
Dmitry Bushev <bushevdv@gmail.com>
2020-09-22 20:27:00 +0300

    DEBUG increase response timeouts

--- src/rust/ide/lib/json-rpc/src/lib.rs
+++ src/rust/ide/lib/json-rpc/src/lib.rs
@@ -36,5 +36,5 @@ pub mod constants {
     use std::time::Duration;
 
     /// The default timeout for all responses.
-    pub const TIMEOUT:Duration = Duration::from_secs(10);
+    pub const TIMEOUT:Duration = Duration::from_secs(60);
 }
